c8a80a119be8789dd568132346ba4a523c508e11
Heloisahsb <108130661+Heloisahsb@users.noreply.github.com>
2023-11-06 08:48:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,57 @@
-alert(`Olá mundo`);
+Function tocaSomPom () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomPom;
+
+
+Function tocaSomClap () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomClap;
+
+Function tocaSomTim () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomTim;
+
+
+Function tocaSomPuff () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomPuff;
+
+Function tocaSomSplash () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomSplash;
+
+Function tocaSomToim () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomToim;
+
+Function tocaSomPsh () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomPsh;
+
+
+Function tocaSomTic () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomTic;
+
+
+Function tocaSomTom () {
+    document.querySelector ('#som_tecla_pom'). play();
+}
+    
+document.querySelector ('.tecla_pom') . onclick = tocaSomTom;
